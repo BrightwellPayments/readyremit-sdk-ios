529f907a137048c22e4895b22db10c948610cb00
Lucas Bordini <lucas.bordini@hotmail.com>
2024-08-01 16:03:36 -0300

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let package = Package(
+    name: "ReadyRemitSDKPackage",
+    products: [
+        .library(
+            name: "ReadyRemitSDK",
+            targets: [
+                "ReadyRemitSDK", 
+                "VisaSensoryBranding",
+                "Swinject"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.7.1"),
+        .package(url: "https://github.com/Swinject/SwinjectAutoRegistration.git", from: "2.8.0"),
+        .package(url: "https://github.com/mixpanel/mixpanel-swift.git", from: "2.9.9"),
+        .package(url: "https://github.com/launchdarkly/ios-client-sdk.git", from: "5.4.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "VisaSensoryBranding",
+            url: "https://github.com/BrightwellPayments/readyremit-sdk-ios/releases/download/v7.2/VisaSensoryBranding.xcframework.zip",
+            checksum: "03ff1cbb5d50dc3d68521506628549d15c26744ecfbf5565d647716bb6118c80"
+        ),
+        .binaryTarget(
+            name: "ReadyRemitSDK",
+            url: "https://github.com/BrightwellPayments/readyremit-sdk-ios/releases/download/v7.2/ReadyRemitSDK.zip",
+            checksum: "10e578c8a92738c64959f079511855de11683c8fd613c2197b1adc8298feecab"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
