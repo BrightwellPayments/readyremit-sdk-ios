fd19015e0b8896a6394766e8202ddbd0f2fb3e22
Lucas Bordini <lucas.bordini@hotmail.com>
2024-10-11 12:37:19 -0300

    Update to v8.1

--- Package.swift
+++ Package.swift
@@ -25,13 +25,13 @@ let package = Package(
                path: "SwiftPM-Exclude/ReadyRemitWrapper"),
         .binaryTarget(
             name: "VisaSensoryBranding",
-            url: "https://github.com/BrightwellPayments/readyremit-sdk-ios/releases/download/v8.0/VisaSensoryBranding.xcframework.zip",
+            url: "https://github.com/BrightwellPayments/readyremit-sdk-ios/releases/download/v8.1/VisaSensoryBranding.xcframework.zip",
             checksum: "03ff1cbb5d50dc3d68521506628549d15c26744ecfbf5565d647716bb6118c80"
         ),
         .binaryTarget(
             name: "ReadyRemitSDK",
-            url: "https://github.com/BrightwellPayments/readyremit-sdk-ios/releases/download/v8.0/ReadyRemitSDK.zip",
-            checksum: "ca56cddb78685983662d5c4897461fdc6708fefc4a63778ff25bcf3c1079d994"
+            url: "https://github.com/BrightwellPayments/readyremit-sdk-ios/releases/download/v8.1/ReadyRemitSDK.zip",
+            checksum: "561b0fa94a78d7ed2bc48735c94fdacfded68c3bc213b71cc839a3e72cca4546"
         )
     ],
     swiftLanguageVersions: [.v5]
